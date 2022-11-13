b63373730f8c79c69a42b37348e381838817e549
Marcus <marcusongkiansiong@gmail.com>
2022-11-14 05:35:13 +0800

    something

--- model/actions.js
+++ model/actions.js
@@ -1,25 +1,25 @@
-// const {Client} = require('pg');
-// const { action } = require('../src/actionOutcomeClass');
+const {Client} = require('pg');
+const { action } = require('../src/actionOutcomeClass');
 
-// const client = new Client({
-//     host: "localhost",
-//     user: "postgres",
-//     port: "5432",
-//     password: "",
-//     database: "accounts"
-// })
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: "5432",
+    password: "",
+    database: "accounts"
+})
 
-// client.connect();
+client.connect();
 
-// const checkData = async (userObject) => {
-//     console.log("------Checking data: Model step------")
-//     console.log(userObject.email)
-//     return await client.query(`select * from users where email = '${userObject.email}'`);
-// }
+const checkData = async (userObject) => {
+    console.log("------Checking data: Model step------")
+    console.log(userObject.email)
+    return await client.query(`select * from users where email = '${userObject.email}'`);
+}
 
-// const insertData = async (userObject) => {
-//     console.log("------Insert data: Model step------")
-//     return await client.query(`insert into users (email,password) values ('${userObject.email}','${userObject.password}')`);
-// }
+const insertData = async (userObject) => {
+    console.log("------Insert data: Model step------")
+    return await client.query(`insert into users (email,password) values ('${userObject.email}','${userObject.password}')`);
+}
 
-// module.exports = {checkData,insertData}
+module.exports = {checkData,insertData}

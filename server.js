835e86cc10c4a16c15696b8367479bae568d83b2
Marcus <marcusongkiansiong@gmail.com>
2022-11-14 15:28:39 +0800

    Comment out all the other routes and require

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const cors = require('cors')
 // require('dotenv').config() 
 
 // importing functionality from controller
-const userClass = require('./src/userClass')
-const actionClass = require('./src/actionOutcomeClass')
-const jwt = require('./src/jwt.js');
-const login = require('./controller/login')
-const register = require('./controller/register')
-const imageKit  = require('./controller/imagekit.js')
+// const userClass = require('./src/userClass')
+// const actionClass = require('./src/actionOutcomeClass')
+// const jwt = require('./src/jwt.js');
+// const login = require('./controller/login')
+// const register = require('./controller/register')
+// const imageKit  = require('./controller/imagekit.js')
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const app = express()
 
 app.use(cors({origin: '*'}))
@@ -21,37 +21,37 @@ app.get('/',(req,res)=>{
     res.send({outcome: "Helllloooooo"})
 })
 
-app.get('/imagekitauthentication',(req,res)=>{
+// app.get('/imagekitauthentication',(req,res)=>{
 
-})
+// })
 
-app.get('/login',(req,res)=>{
-    console.log("------Processing Login: Server step------")
+// app.get('/login',(req,res)=>{
+//     console.log("------Processing Login: Server step------")
 
-    const user = new userClass.User(req.query);         // Filled
-    const action = new actionClass.action("login");     // Empty            -> What you are going to return
-    const jwtToken = new jwt.jwtObject();               // Empty
+//     const user = new userClass.User(req.query);         // Filled
+//     const action = new actionClass.action("login");     // Empty            -> What you are going to return
+//     const jwtToken = new jwt.jwtObject();               // Empty
 
-    login.checkLogin(user,action,jwtToken)
-    .then(outcome=>{
-        console.log('Final Outcome: ',action.getStatus())
-        console.log("------Processing Login: End------")
-        res.send(action.getStatus())
-    })
-})
+//     login.checkLogin(user,action,jwtToken)
+//     .then(outcome=>{
+//         console.log('Final Outcome: ',action.getStatus())
+//         console.log("------Processing Login: End------")
+//         res.send(action.getStatus())
+//     })
+// })
 
-app.get('/register',(req,res)=>{
-    console.log("------Processing Register: Server step------")
-    const user = new userClass.User(req.query);         // Filled
-    const action = new actionClass.action("register");     // Empty            -> What you are going to return
-    const jwtToken = new jwt.jwtObject();               // Empty
+// app.get('/register',(req,res)=>{
+//     console.log("------Processing Register: Server step------")
+//     const user = new userClass.User(req.query);         // Filled
+//     const action = new actionClass.action("register");     // Empty            -> What you are going to return
+//     const jwtToken = new jwt.jwtObject();               // Empty
 
-    // Tackle
-    register.register(user,action,jwtToken)
-    .then(respond=>{
-        res.send(action.getStatus())
-    })
-})
+//     // Tackle
+//     register.register(user,action,jwtToken)
+//     .then(respond=>{
+//         res.send(action.getStatus())
+//     })
+// })
 
 app.get('/test',(req,res)=>{
     console.log("------Processing test: Server step------")
@@ -59,39 +59,39 @@ app.get('/test',(req,res)=>{
     res.send({outcome: "heeloing you have tested it"})
 })
 
-const data = {name: "marcus",photo: "something", email: "marcus@gmail.com"}
+// const data = {name: "marcus",photo: "something", email: "marcus@gmail.com"}
 
-app.get('/profiledata',(req,res)=>{
-    console.log("------Processing profile data: Server step------")
-    console.log(req.query)
-    res.send({
-        action: "profile data",
-        outcome: true,
-        data: data
-    })
-})
+// app.get('/profiledata',(req,res)=>{
+//     console.log("------Processing profile data: Server step------")
+//     console.log(req.query)
+//     res.send({
+//         action: "profile data",
+//         outcome: true,
+//         data: data
+//     })
+// })
 
-app.get('/updateprofiledata',(req,res)=>{
-    // I need the token, and I need to query
-    // 1. Check token 
-    // 2. Check 
-    console.log(req.query)
+// app.get('/updateprofiledata',(req,res)=>{
+//     // I need the token, and I need to query
+//     // 1. Check token 
+//     // 2. Check 
+//     console.log(req.query)
 
-    // assume token has been checked
+//     // assume token has been checked
 
 
-    for (const key in req.query){
-        if (key !== "token"){
-            data[key] = req.query[key]
-        }
-    }
+//     for (const key in req.query){
+//         if (key !== "token"){
+//             data[key] = req.query[key]
+//         }
+//     }
 
-    res.send({
-        action: "update profile data", 
-        outcome: true,
-        data: data
-    })
-})
+//     res.send({
+//         action: "update profile data", 
+//         outcome: true,
+//         data: data
+//     })
+// })
 
 
 

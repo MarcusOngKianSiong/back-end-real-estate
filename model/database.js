64e3c5b26e5ae3b26b1da080fd8bf70e68ccc87b
Marcus <marcusongkiansiong@gmail.com>
2022-11-15 18:48:08 +0800

    testing database connection between server and database

---
+++ model/database.js
@@ -0,0 +1,17 @@
+const {Client} = require('pg');
+
+// --host=ec2-52-1-17-228.compute-1.amazonaws.com 
+// --port=5432 
+// --username=xpflcriillmvkv 
+// --password 
+// --dbname=d1rlatk18qeqg2 
+
+const herokuClient = new Client({
+    host: "ec2-52-1-17-228.compute-1.amazonaws.com",
+    user: "xpflcriillmvkv",
+    port: "5432",
+    password: "9ddaa18cc6e096002291d245f91dfd1c4098445bf6add7d181dc3ca81e2a3f2e",
+    database: "d1rlatk18qeqg2"
+})
+
+herokuClient.connect()
